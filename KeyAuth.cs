d6156b4c2e6312a2abc7a96fe8bb7a13662e72c9
xpe.nettt <martinezsosayostinesfrain4@gmail.com>
2026-04-05 16:06:05 -0400

    Add KeyAuth class with application details

---
+++ KeyAuth.cs
@@ -0,0 +1,4 @@
+public static string appName = "Spotify Premium";
+public static string ownerId = "wFjO3TFRgb";
+public static string secret = "4c1cf2391214809c7c5a867ab494e4950be326e808d4a345d84116941d62aefd";
+public static string version = "1.0";
